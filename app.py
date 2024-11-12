204e8636cedb05af6d4a84a757f1305d916c11d1
Matthew Martin <334606@edu.mkcollege.ac.uk>
2024-11-12 12:11:51 +0000

    added many things

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import hashlib
 import sqlite3
-
+import string
+from flask_session import Session
+acceptedChars = [x for x in string.punctuation+string.ascii_letters+string.digits]
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
 
 @app.route("/")
 def index():
@@ -12,13 +19,23 @@ def index():
 @app.route("/register", methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
-        username = request.form.get('username')
         password = request.form.get('password')
         email = request.form.get('email')
-        print(username, password, email)
-        if len(password) <= 7:
+        username = request.form.get('username')
+        ConfirmPass = request.form.get('password2')
+        if len(password) <= 7 :
             return render_template("error.html", error="Password must be at least 8 characters long")
-        hashed = hashlib.sha256(password.encode()).hexdigest()
+        if password != ConfirmPass:
+            return render_template("error.html", error="Passwords do not match")
+        for z in password:
+            if z not in acceptedChars:
+                return render_template("error.html", error="Password contains characters that are not allowed")
+        if len(username) <= 6 or len(username) >= 13:
+            return render_template("error.html", error="Username must be between 6 and 12 characters long")
+        for z in username:
+            if z not in acceptedChars:
+                return render_template("error.html", error="Username contains characters that are not allowed")
+        hashed = hashlib.sha512(password.encode()).hexdigest()
         conn = sqlite3.connect('database.db')
         c = conn.cursor()
         c.execute('''
@@ -27,7 +44,8 @@ def register():
             ''', (username, hashed, email))
         conn.commit()
         conn.close()
-        #return redirect(url_for("login"))
+        session["username"] = request.form.get("username")
+        return render_template("welcome.html", username=session['username'])
     return render_template("register.html")
 
 
@@ -36,17 +54,27 @@ def login():
     if request.method == "POST":
         username = request.form.get('username')
         password = request.form.get('password')
-        if len(password) <= 7:
-            return render_template("error.html", error="Password must be at least 8 characters long")
-        hashed = hashlib.sha256(password.encode()).hexdigest()
+        hashed = hashlib.sha512(password.encode()).hexdigest()
         conn = sqlite3.connect('database.db')
         c = conn.cursor()
-        c.execute("SELECT * FROM LoginDetails WHERE Username = ?", (username,hashed))
+        logCheck = c.execute('SELECT * FROM LoginDetails WHERE Username = ? AND Password = ?',(username, hashed)).fetchone()
+        if logCheck is None:
+            return render_template("error.html", error="Invalid username or password")
+        conn.close()
+        session["username"] = request.form.get("username")
+        return render_template('index.html', username=session.get('username'))
     return render_template("login.html")
     
 @app.route("/welcome")
 def welcome():
     return render_template("welcome.html", username=session['username'])
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
